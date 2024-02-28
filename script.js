c7cd584cfecf8632acf1c3588219f8d580447ec6
Yasmin0209 <160732275+Yasmin0209@users.noreply.github.com>
2024-02-28 09:04:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 30;
+
+
+
+function setup() {  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+
+   circle(xBolinha,yBolinha);
+}
